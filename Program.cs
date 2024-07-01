e0617c536d25b11caa6b60d1e94dbe3a626ec586
Amanda Hestnes <a-hestnes@hotmail.com>
2024-07-01 21:19:15 +0200

    Added main program. Added selection logic.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DepositMachine machine = new DepositMachine();
+
+            Console.WriteLine("Can/Bottle Deposit Machine");
+            Console.WriteLine("1. Turn in a bottle");
+            Console.WriteLine("2. Turn in a can");
+            Console.WriteLine("3. Print voucher");
+            Console.WriteLine("4. Show log");
+            Console.WriteLine("5. Exit");
+
+            while (true) 
+            {
+                Console.WriteLine("Choose an option");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        await machine.AcceptBottleAsync();
+                        break;
+                    case "2":
+                        await machine.AcceptCanAsync();
+                        break;
+                    case "3":
+                        machine.PrintVoucher();
+                        break;
+                    case "4":
+                        machine.ShowLog();
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        break;
+                }
+            }
+
+            
         }
     }
 }
