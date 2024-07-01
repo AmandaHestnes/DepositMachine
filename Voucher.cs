732f2c55eb6c2641ba40de399a0fa948fc7a055d
Amanda Hestnes <a-hestnes@hotmail.com>
2024-07-01 20:35:00 +0200

    added basic voucher logic

---
+++ Voucher.cs
@@ -0,0 +1,17 @@
+﻿namespace DepositMachine
+{
+    internal class Voucher
+    {
+        public int Amount {  get; private set; }
+
+        public Voucher(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        public void PrintVoucher()
+        {
+            Console.WriteLine("Voucher Amount: {Amount} NOK");
+        }
+    }
+}
