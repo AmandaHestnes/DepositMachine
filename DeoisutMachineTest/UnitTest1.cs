77938a847919ee5d231aeddd5b5b5b6d5addf77f
Amanda Hestnes <amanda.hestnes@rema.no>
2024-07-02 08:32:15 +0200

    Changed file structure, so test project is on correct place.

---
+++ DeoisutMachineTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DeoisutMachineTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
